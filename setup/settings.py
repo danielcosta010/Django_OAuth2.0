0d80b9f117ac081eef09471b1cb4d60a0ef5f8a4
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-10-19 23:26:19 -0300

    Adicionando client_id e secret

--- setup/settings.py
+++ setup/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 
 # Application definition
 
@@ -141,8 +143,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SOCIALACCOUNT_PROVIDERS = {
     'github': {
         'APP': {
-            'client_id': '123',
-            'secret': '456',
+            'client_id': 'Ov23liV9s3UoxCquhWu7',
+            'secret': 'ea40a4f0e3383a8d709b482ef4f83ce614c3aff8',
             'key': ''
         }
     }
